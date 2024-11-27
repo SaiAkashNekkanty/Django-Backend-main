574b0d98a0f76935cf66b882f598d8d7f9c92232
Sai Akash Nekkaty <nekkanty.s@northeastern.edu>
2024-11-27 10:12:45 -0500

    Module changes

--- micro-ecommerce/config/wsgi.py
+++ micro-ecommerce/config/wsgi.py
@@ -12,6 +12,6 @@ import sys
 print(sys.path)
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'micro-ecommerce.config.settings')
 
 application = get_wsgi_application()
