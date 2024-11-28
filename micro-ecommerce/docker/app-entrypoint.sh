1f68b48aabbf9126afcd424dc6c12fd8fc661bb7
Sai Akash Nekkaty <nekkanty.s@northeastern.edu>
2024-11-27 20:15:25 -0500

    deployment

--- micro-ecommerce/docker/app-entrypoint.sh
+++ micro-ecommerce/docker/app-entrypoint.sh
@@ -6,10 +6,14 @@ if [ ! -f ".env" ]; then
   cp .env.example .env
 fi
 
-dockerize -wait tcp://database:5432 -wait tcp://rabbitmq:15672 -timeout 2700s -wait-retry-interval 10s
+set -e
+dockerize -wait tcp://database:5432 -wait tcp://rabbitmq:5672 -timeout 30s
+exec "$@"
 
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata fake_data 
 python manage.py loaddata initial_data 
 python manage.py runserver 0.0.0.0:8000
+
+exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
